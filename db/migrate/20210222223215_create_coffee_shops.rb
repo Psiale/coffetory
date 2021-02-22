5d0e8b447a31a6d55000dea8c2e5b90b45c002bf
psialedev <psialedev@gmail.com>
2021-02-22 16:34:09 -0600

    added coffee shop and owner models

---
+++ db/migrate/20210222223215_create_coffee_shops.rb
@@ -0,0 +1,11 @@
+class CreateCoffeeShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coffee_shops do |t|
+      t.string :name
+      t.text :avatar
+      t.references :owner, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
