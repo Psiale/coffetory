1fa4dcc8b619eb754fefc2609a95052d2c46084b
psialedev <psialedev@gmail.com>
2021-02-23 13:04:32 -0600

    auth not working. debugging

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,7 +1,8 @@
 class CoffeeShop < ApplicationRecord
   belongs_to :owner
-  has_many :raw_materials
-  has_many :products
+  has_many :raw_materials, dependent: :destroy
+  has_many :products, dependent: :destroy
+  validates_presence_of :name
   #I have to figure how to do the ingredients relationship
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,5 @@
 class Owner < ApplicationRecord
-has_many :coffee_shops
+  has_secure_password
+  has_many :coffee_shops
+  validates_presence_of :name, :email, :password_digest
 end
