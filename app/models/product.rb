ad0ec76336c2b68c58302c686020b4fb4491381e
psialedev <psialedev@gmail.com>
2021-02-24 13:50:03 -0600

    fixed type in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :raw_materials
   belongs_to :coffee_shop
-  validate_presence_of :name
-  validate_presence_of :cost
+  validates_presence_of :name
+  validates_presence_of :cost
 end
