c35f7cb1165db94ad14756d2d9f940ec638b4ef7
psialedev <psialedev@gmail.com>
2021-03-05 11:39:18 -0600

    Validate name uniqueness for produt and business

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -3,6 +3,6 @@ class CoffeeShop < ApplicationRecord
   has_many :raw_materials, dependent: :destroy
   has_many :products, dependent: :destroy
   validates_presence_of :name
-  #I have to figure how to do the ingredients relationship
+  validates_uniqueness_of :name, {:scope => :owner_id}
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :raw_materials
   belongs_to :coffee_shop
   validates_presence_of :name
+  validates_uniqueness_of :name, {:scope => :coffee_shop_id}
   validates_presence_of :cost
   
 end
