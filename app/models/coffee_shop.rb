0170538ed58bfa221ec3e73bad47c40468833e47
psialedev <psialedev@gmail.com>
2021-03-19 18:05:19 -0600

    Fix fix fix

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -3,5 +3,5 @@ class CoffeeShop < ApplicationRecord
   has_many :raw_materials, dependent: :destroy
   has_many :products, dependent: :destroy
   validates_presence_of :name
-  validates_uniqueness_of :name, { :scope => :owner_id }
+  validates_uniqueness_of :name, {scope: :owner_id }
 end
