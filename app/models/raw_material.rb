71ba4e48efda2f010096383bf94aec85b6d71973
psialedev <psialedev@gmail.com>
2021-03-19 18:07:56 -0600

    FIXING

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -2,7 +2,7 @@ class RawMaterial < ApplicationRecord
   belongs_to :coffee_shop
   has_and_belongs_to_many :products
   validates_presence_of :name
-  validates_uniqueness_of :name, { :scope => :coffee_shop_id }
+  validates_uniqueness_of :name, { scope: :coffee_shop_id }
   validates_presence_of :total_amount
   validates_presence_of :remaining_amount
 end
