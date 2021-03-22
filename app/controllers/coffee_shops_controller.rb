140b974b0d068054a8fb9d15f08b171736cec18c
psialedev <psialedev@gmail.com>
2021-03-22 11:15:25 -0600

    Changed instance vor local variable on coffe shops controller#

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -1,26 +1,26 @@
 class CoffeeShopsController < ApplicationController
   before_action :set_coffee_shop, only: [:show, :update, :destroy]
   def index
-    @coffee_shop = CoffeeShop.where(owner_id: @current_owner).first
-    json_response(@coffee_shop)
+    coffee_shop = CoffeeShop.where(owner_id: @current_owner).first
+    json_response(coffee_shop)
   end
 
   def create
-    @coffee_shop = current_owner.coffee_shops.create!(coffee_shop_params)
-    json_response(@coffee_shop, :created)
+    coffee_shop = current_owner.coffee_shops.create!(coffee_shop_params)
+    json_response(coffee_shop, :created)
   end
 
   def show
-    json_response(@coffee_shop)
+    json_response(coffee_shop)
   end
 
   def update
-    @coffee_shop.update(coffee_shop_params)
+    coffee_shop.update(coffee_shop_params)
     head :no_content
   end
 
   def destroy
-    @coffee_shop.destroy
+    coffee_shop.destroy
     head :no_content
   end
 
@@ -31,6 +31,6 @@ class CoffeeShopsController < ApplicationController
   end
 
   def set_coffee_shop
-    @coffee_shop = CoffeeShop.find(params[:id])
+    coffee_shop = CoffeeShop.find(params[:id])
   end
 end
