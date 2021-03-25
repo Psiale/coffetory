13e139e8e65c152c3b42066af53abaf978a6ab7b
psialedev <psialedev@gmail.com>
2021-03-25 09:17:13 -0600

    changed authentication routes and controller for sessions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,6 @@
-class AuthenticationController < ApplicationController
+class SessionController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
-  def authenticate
+  def create
     auth_token =
       AuthenticateOwner.new(auth_params[:email], auth_params[:password]).call
     json_response(auth_token: auth_token)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :coffee_shops, :path => 'business' do
     resources :raw_materials, except: :show
   end
-  post 'sessions', to: 'authentication#authenticate'
+  resources :sessions, only: :create
   post 'signup', to: 'owners#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
